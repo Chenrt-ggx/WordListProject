fde87eaaa2882321ac8d507f5254a3a6b88c56ff
Dr-Bluemond <dr_bluemond@126.com>
2022-03-24 22:00:10 +0800

    check cycle

--- src/core.cpp
+++ src/core.cpp
@@ -1,4 +1,5 @@
 #include "core.h"
+#include "global.h"
 
 #include <vector>
 #include <cstring>
@@ -48,7 +49,7 @@ struct Node
 
 vector<Node> graph[26][26];
 
-static void build(vector<char*> words)
+static void build(vector<char*> words, bool enable_loop)
 {
 	for (char* i : words)
 	{
@@ -60,43 +61,102 @@ static void build(vector<char*> words)
 		for (int j = 0; j < 26; j++)
 		{
 			sort(graph[i][j].begin(), graph[i][j].end());
-			graph[i][j].erase(unique(graph[i][j].begin(), graph[i][j].end()), graph[i][j].end());
+			if (enable_loop)
+			{
+				graph[i][j].erase(unique(graph[i][j].begin(), graph[i][j].end()), graph[i][j].end());
+			}
 		}
 	}
+	//for (int i = 0; i < 26; i++)
+	//{
+	//	for (int j = 0; j < 26; j++)
+	//	{
+	//		printf("(%c, %c): ", i + 'a', j + 'a');
+	//		for (Node i : graph[i][j])
+	//		{
+	//			printf("%s ", i.word);
+	//		}
+	//		puts("");
+	//	}
+	//}
+	if (!enable_loop)
+	{
+		check_cycle();
+	}
+}
+
+constexpr int WHITE = 0;
+constexpr int GRAY = 1;
+constexpr int BLACK = 2;
+
+int status[26];
+
+bool check_dfs(int parent)
+{
 	for (int i = 0; i < 26; i++)
 	{
-		for (int j = 0; j < 26; j++)
+		if (parent == i)
 		{
-			printf("(%c, %c): ", i + 'a', j + 'a');
-			for (Node i : graph[i][j])
+			continue;
+		}
+		if (!graph[parent][i].empty())
+		{
+			if (status[i] == WHITE)
 			{
-				printf("%s ", i.word);
+				status[i] = GRAY;
+				if (check_dfs(i))
+				{
+					return true;
+				}
+				status[i] = BLACK;
 			}
-			puts("");
+			else if (status[i] == GRAY)
+			{
+				return true;
+			}
 		}
 	}
+	return false;
+}
+
+void check_cycle() 
+{
+	memset(status, 0, sizeof(status));
+	for (int i = 0; i < 26; i++)
+	{
+
+		if (status[i] == WHITE && check_dfs(i))
+		{
+			throw E_WORD_CYCLE_EXIST;
+		}
+		if (graph[i][i].size() > 1)
+		{
+			throw E_WORD_CYCLE_EXIST;
+		}
+	}
+	
 }
 
 vector<char*> gen_chain_word(vector<char*> words, char head, char tail, bool enable_loop)
 {
-	build(words);
+	build(words, enable_loop);
 	return vector<char*>(1);
 }
 
 vector<char*> gen_chains_all(vector<char*> words)
 {
-	build(words);
+	build(words, false);
 	return vector<char*>(1);
 }
 
 vector<char*> gen_chain_word_unique(vector<char*> words)
 {
-	build(words);
+	build(words, false);
 	return vector<char*>(1);
 }
 
 vector<char*> gen_chain_char(vector<char*> words, char head, char tail, bool enable_loop)
 {
-	build(words);
+	build(words, enable_loop);
 	return vector<char*>(1);
 }
--- src/core.h
+++ src/core.h
@@ -11,3 +11,9 @@ vector<char*> gen_chains_all(vector<char*> words);
 vector<char*> gen_chain_word_unique(vector<char*> words);
 
 vector<char*> gen_chain_char(vector<char*> words, char head, char tail, bool enable_loop);
+
+static void build(vector<char*> words, bool enable_loop);
+
+void check_cycle();
+
+bool check_dfs(int parent);
