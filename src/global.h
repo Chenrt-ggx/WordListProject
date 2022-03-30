589e0abdbdb1b54476ccd5c11faa8820df8ba0b6
Chenrt <2926777985@qq.com>
2022-03-30 21:55:27 +0800

    use dll in src

--- src/global.h
+++ src/global.h
@@ -1,18 +1,13 @@
 #pragma once
 
+constexpr int E_FLAG_UNDEFINE = 0x80000002;
+constexpr int E_FLAG_CONFLICT = 0x80000003;
+constexpr int E_FLAG_NOT_EXIST = 0x80000004;
+constexpr int E_FLAG_PARAM_FORMAT_ERROR = 0x80000005;
+constexpr int E_FLAG_PARAM_NOT_EXIST = 0x80000006;
 
-constexpr int E_FLAG_UNDEFINE = 1;
-constexpr int E_FLAG_CONFLICT = 2;
-constexpr int E_FLAG_NOT_EXIST = 3;
-constexpr int E_FLAG_PARAM_FORMAT_ERROR = 4;
-constexpr int E_FLAG_PARAM_NOT_EXIST = 5;
-
-constexpr int E_FILENAME_NOT_PROVIDE = 6;
-constexpr int E_FILENAME_DUPLICATE = 7;
-constexpr int E_FILE_NOT_EXIST = 8;
-constexpr int E_FILE_UNABLE_TO_OPEN = 9;
-constexpr int E_MEMORY_ALLOC_FAIL = 10;
- 
-constexpr int E_WORD_CYCLE_EXIST = 11;
-
-constexpr int MAX_RESULT_LENGTH = 20000;
+constexpr int E_FILENAME_NOT_PROVIDE = 0x80000007;
+constexpr int E_FILENAME_DUPLICATE = 0x80000008;
+constexpr int E_FILE_NOT_EXIST = 0x80000009;
+constexpr int E_FILE_UNABLE_TO_OPEN = 0x80000010;
+constexpr int E_MEMORY_ALLOC_FAIL = 0x80000011;
--- src/src.cpp
+++ src/src.cpp
@@ -1,17 +1,21 @@
-﻿#include <cstdio>
+﻿#include "global.h"
+#include "../core/core.h"
+
+#include <cstdio>
 #include <cctype>
 #include <cstring>
 #include <cstdlib>
 #include <vector>
 
-#include "core.h"
-#include "global.h"
-
-#include "../core/core.h"
 #pragma comment(lib, "../x64/Debug/core.lib")
 
-_declspec(dllimport) int add(int a, int b);
-_declspec(dllimport) int mul(int a, int b);
+extern "C" _declspec(dllimport) int gen_chain_word(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
+
+extern "C" _declspec(dllimport) int gen_chains_all(char* words[], int len, char* result[]);
+
+extern "C" _declspec(dllimport) int gen_chain_word_unique(char* words[], int len, char* result[]);
+
+extern "C" _declspec(dllimport) int gen_chain_char(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
 
 using namespace std;
 
@@ -253,7 +257,6 @@ inline void call_core()
 
 int main(int argc, char* argv[])
 {
-    printf("%d %d", add(1, 2), mul(1, 2));
     try
     {
         parse_params(argc, argv);
